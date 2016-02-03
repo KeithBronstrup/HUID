a1bcd6f45bf947e44bce336b6cd6cf06f63bca44
Keith Bronstrup <keith@bronstrup.com>
2016-02-03 10:52:01 -0800

    Update PHP Reference implementation
    Updated to reflect nomenclature change (Host ID -> Namespace) and HUID format change.

--- reference/php/huid.php
+++ reference/php/huid.php
@@ -3,11 +3,11 @@
 /** 
  * Generate a Host Unique Identifier
  * 
- * @param string $primaryID    The primary host ID, 4 hexadecimal characterd
- * @param string $secondaryID  The secondary host ID, 3 hexadecimal characters
+ * @param string $primaryNS    The primary namespace, 4 hexadecimal characterd
+ * @param string $secondaryNS  The secondary namespace, 3 hexadecimal characters
  * @param string $format       The desired output format, one of 'str', 'hex', 'bin', or 'obj'. Default 'str'
- *                              - str: Return a 36 character string in the format AAAA-BBB-CCCC-DDDDDDDDDDDDDD-EEEEEEE
- *                              - hex: Return a 32 digit hexadecimal value in the format AAAABBBCCCCDDDDDDDDDDDDDDEEEEEEE
+ *                              - str: Return a 36 character string in the format AAAAAAAAAAAAAA-BBBBBBB-CCCC-DDD-EEEE
+ *                              - hex: Return a 32 digit hexadecimal value in the format AAAAAAAAAAAAAABBBBBBBCCCCDDDEEEE
  *                              - bin: Return a 16 byte binary string
  *                              - obj: Return an object containing all 3 formats
  * 
@@ -15,11 +15,11 @@
  * @example /reference/php/example.php
  */
 
-function getHUID ($primaryID, $secondaryID, $format = 'str') {
-	if (strlen($primaryID) === 4
-	 && strlen($secondaryID) === 3
-	 && ctype_xdigit($primaryID)
-	 && ctype_xdigit($secondaryID))
+function getHUID ($primaryNS, $secondaryNS, $format = 'str') {
+	if (strlen($primaryNS) === 4
+	 && strlen($secondaryNS) === 3
+	 && ctype_xdigit($primaryNS)
+	 && ctype_xdigit($secondaryNS))
     {
 		if (in_array($format, ['hex','bin']))
 		{
@@ -29,15 +29,15 @@ function getHUID ($primaryID, $secondaryID, $format = 'str') {
 		}
 		
 		$huid = strtolower(
-			$primaryID.
-			$delimiter.
-			$secondaryID.
-			$delimiter.
-			str_pad(dechex(mt_rand(0,0xffff)), 4, '0', STR_PAD_LEFT).
-			$delimiter.
 			str_pad(dechex(time()), 14, '0', STR_PAD_LEFT).
 			$delimiter.
-			str_pad(dechex(substr(microtime(), 2, 8)), 7, '0', STR_PAD_LEFT)
+			str_pad(dechex(substr(microtime(), 2, 8)), 7, '0', STR_PAD_LEFT).
+			$delimiter.
+			$primaryNS.
+			$delimiter.
+			$secondaryNS.
+			$delimiter.
+			str_pad(dechex(mt_rand(0,0xffff)), 4, '0', STR_PAD_LEFT)
 		);
 
 		switch ($format)
